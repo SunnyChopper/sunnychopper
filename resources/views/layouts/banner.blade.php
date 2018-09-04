5ce2d6daa304b921063607218d22460067475190
SunnyChopper <ishy.singh@gmail.com>
2018-09-04 11:35:22 -0500

    Finished all basic major layouts

---
+++ resources/views/layouts/banner.blade.php
@@ -0,0 +1,12 @@
+<!-- start banner Area -->
+<section class="banner-area relative" id="top-banner">
+	<div class="overlay overlay-bg"></div>
+	<div class="container">				
+		<div class="row d-flex align-items-center justify-content-center">
+			<div class="about-content col-lg-12">
+				<h1 class="text-white">{{ $page_header }}</h1>	
+			</div>	
+		</div>
+	</div>
+</section>
+<!-- End banner Area -->
