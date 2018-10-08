7729cbb0f7d7be1854e3b73135846399a4a6f7a8
SunnyChopper <ishy.singh@gmail.com>
2018-10-08 00:52:10 -0500

    Updated Tools page

--- resources/views/pages/tools.blade.php
+++ resources/views/pages/tools.blade.php
@@ -9,7 +9,14 @@
 				<h3>Software Tools for Entrepreneurs</h3>
 				<hr />
 				<div class="well">
-					<p class="text-center mb-0">Currently, I don't have any public tools. Check back later or follow my YouTube channel for updates.</p>
+					<ul class="list-group">
+						<li class="list-group-item p-32">
+							<h4 class="mb-0">OptinDev</h4>
+							<hr />
+							<p class="mb-2">An opt-in page builder based on psychology to help you build a contact list for marketing purposes. Easily create opt-in pages that you can then forward to MailChimp, Aweber, or your favorite email service provider. Also have included support for ManyChat for Facebook Messenger marketing.</p>
+							<a href="https://optindev.com" class="btn btn-sm btn-primary">Access OptinDev</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>
