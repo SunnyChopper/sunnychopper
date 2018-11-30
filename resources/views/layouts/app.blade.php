bedeae3a816dbcde47cae192d700a0e86aafe971
Sunny Singh <ishy.singh@gmail.com>
2018-11-29 21:46:10 -0600

    Removed unused CSS for speed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,15 +58,9 @@
 		@endif
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-		
-
 		<link rel="stylesheet" href="{{ URL::asset('css/linearicons.css') }}">
 		<link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
-		<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">
-		<link rel="stylesheet" href="{{ URL::asset('css/magnific-popup.css') }}">
-		<link rel="stylesheet" href="{{ URL::asset('css/nice-select.css') }}">							
-		<link rel="stylesheet" href="{{ URL::asset('css/animate.min.css') }}">
-		<link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">		
 		<link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
 		<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 
