9953ede087e1edc0933a9bd9eb20e1a10a9ca7aa
Sunny Singh <ishy.singh@gmail.com>
2018-12-06 02:44:24 -0600

    Almost there

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,8 +1,9 @@
 self.addEventListener('push', function(event) {
 	if (event.data) {
+		console.log(event);
 		// var data = event.data.json();
 		self.registration.showNotification("Testing",{
-			body: "This is a test"
+			body: event.data.text()
 		});
 		console.log('This push event has data: ', event.data.text());
 	} else {
