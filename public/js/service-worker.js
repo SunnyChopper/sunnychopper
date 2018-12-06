29c5a1107b11b86647345a8dbe8d11260802e18c
Sunny Singh <ishy.singh@gmail.com>
2018-12-06 02:01:43 -0600

    I really need to install gmp on the local machine

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,11 +1,13 @@
 self.addEventListener('push', function (e) {
+	console.log('Got the listener');
     if (!(self.Notification && self.Notification.permission === 'granted')) {
         //notifications aren't supported or permission not granted!
+        console.log('Not granted');
         return;
     }
     if (e.data) {
         var msg = e.data.json();
-        console.log(msg)
+        console.log(msg);
         e.waitUntil(self.registration.showNotification(msg.title, {
             body: msg.body,
             icon: msg.icon,
