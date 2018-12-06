1def8c10d7a71475be2d15052c568b1c48703233
Sunny Singh <ishy.singh@gmail.com>
2018-12-06 02:22:19 -0600

    Almost there

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,16 +1,11 @@
-self.addEventListener('push', function (e) {
-	console.log('Got the listener');
-    if (!(self.Notification && self.Notification.permission === 'granted')) {
-        //notifications aren't supported or permission not granted!
-        console.log('Not granted');
-        return;
-    }
-    if (e.data) {
-    	console.log(e.data.json());
-        var msg = e.data.json();
-        
-        e.waitUntil(self.registration.showNotification(msg.title, {
-            body: msg.body
-        }));
-    }
+self.addEventListener('push', function(event) {
+	if (event.data) {
+		var data = event.data.json();
+		self.registration.showNotification(data.title,{
+			body: data.body
+		});
+		console.log('This push event has data: ', event.data.text());
+	} else {
+		console.log('This push event has no data.');
+	}
 });
