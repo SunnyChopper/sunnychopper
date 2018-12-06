b07023fe1036e49e2661067f69450951d9b854d3
Sunny Singh <ishy.singh@gmail.com>
2018-12-06 04:28:12 -0600

    This is basically done

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener('push', function(event) {
 	if (event.data) {
 		console.log(JSON.parse(event.data.text()));
 		// var data = event.data.json();
-		self.registration.showNotification("Testing",{
-			body: event.data.text()
+		self.registration.showNotification(data.title,{
+			body: data.body
 		});
 		console.log('This push event has data: ', event.data.text());
 	} else {
