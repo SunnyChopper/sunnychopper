2023c664b8bb9503ba9249e13470e948baad89eb
Sunny Singh <ishy.singh@gmail.com>
2018-12-13 19:53:18 -0600

    Debugging mode

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,6 +1,7 @@
 self.addEventListener('push', function(event) {
 	if (event.data) {
 		var data = JSON.parse(event.data.text());
+		console.log(data);
 		self.registration.showNotification(data.title,{
 			body: data.body,
 			data: {
