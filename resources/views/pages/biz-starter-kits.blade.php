e3476a14b6f50a8f945c8f215a740590dc6f2661
Sunny Singh <ishy.singh@gmail.com>
2019-09-19 15:13:29 -0500

    Added Facebook Pixel to landing page

--- resources/views/pages/biz-starter-kits.blade.php
+++ resources/views/pages/biz-starter-kits.blade.php
@@ -126,6 +126,22 @@
 				}
 			}
 		</style>
+
+		<script>
+			!function(f,b,e,v,n,t,s)
+			{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window, document,'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '1225441154310154');
+			fbq('track', 'PageView');
+		</script>
+		<noscript>
+			<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1225441154310154&ev=PageView&noscript=1"/>
+		</noscript>
 	</head>
 	<body>
 		<!-- Google Tag Manager (noscript) -->
@@ -364,6 +380,7 @@
 						},
 						success : function(data) {
 							if (data == 1) {
+  								fbq('track', 'Lead');
 								$('#submit_error').hide();
 								$('#submit_email').val('');
 								$('#submit_email').css('border', '1px solid green');
